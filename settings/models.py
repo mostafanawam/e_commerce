d81bf5fca40f7c398e88ac73c98abb332b5cba49
mynawam <mynawam@bluemint.ai>
2023-09-08 22:23:04 +0300

    update checkout

--- settings/models.py
+++ settings/models.py
@@ -5,6 +5,7 @@ class Settings(models.Model):
     currency=models.CharField(max_length=5,default='$')
     delivery=models.DecimalField(decimal_places=2,max_digits=5,default=3)
 
+
     # def __str__(self):
     #     return self.name
 
